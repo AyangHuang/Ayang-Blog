b90951d0258dad68abb3b917ed948cfb2ff7d411
Ayang <3392406201@qq.com>
2022-10-18 12:56:57 +0800

    update auto

--- autodeploy/autodeploy.sh
+++ autodeploy/autodeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ayang/site/Ayang-Blog
 git pull origin main
+git submodule update --remote themes/FixIt # 更新子项目
 /home/ayang/go/bin/hugo
 echo $(date "+%Y-%m-%d %H:%M:%S") >> autodeploy/autodeploy.log
