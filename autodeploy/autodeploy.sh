700c826799a1f2eeafb35dfb434a4ae463a22bdd
Ayang <3392406201@qq.com>
2023-02-14 21:24:28 +0800

    修改主页为post

--- autodeploy/autodeploy.sh
+++ autodeploy/autodeploy.sh
@@ -2,5 +2,6 @@
 cd /home/ayang/site/Ayang-Blog
 git pull origin main
 git submodule update --remote themes/FixIt # 更新子项目
-/home/ayang/go/bin/hugo
+/home/ayang/go/bin/hugo  # 执行hugo构建静态网站
+cp /home/ayang/site/Ayang-Blog/public/posts/index.html /home/ayang/site/Ayang-Blog/public/index.html # 把 posts 作为主页
 echo $(date "+%Y-%m-%d %H:%M:%S") >> autodeploy/autodeploy.log
